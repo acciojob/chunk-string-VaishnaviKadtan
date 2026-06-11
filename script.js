b28b4b859dc80eba2d164c77de4c3de98ba3b2e3
VaishnaviKadtan <deepikakadtan15@gmail.com>
2026-06-11 12:27:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function stringChop(str, size) {
   // your code here
+	stringChop("Hello, world!", 5);
+// ["Hello", ", wor", "ld!"]
+
+stringChop("12345", 2);
+// ["12", "34", "5"]
+
+stringChop("abc", 5);
+// ["abc"]
+
+stringChop(null, 3);
+	
+// []
 }
 
 // Do not change the code below
